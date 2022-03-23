c375b4c703c9022869ef239242c66974fb3c2b62
Armand94290 <armandboudin94290@gmail.com>
2022-03-23 22:32:31 +0100

    test

--- test2.py
+++ test2.py
@@ -1,36 +1,30 @@
 import math
 from simpleai.search import SearchProblem, astar
 
-# Class containing the methods to solve the maze
-
 
 class MazeSolver(SearchProblem):
-    # Initialize the class
     def __init__(self, board):
         self.board = board
         self.goal = (0, 0)
 
         for y in range(len(self.board)):
             for x in range(len(self.board[y])):
-                if self.board[y][x].lower() == "o":
+                if self.board[y][x].lower() == "1":
                     self.initial = (x, y)
-                elif self.board[y][x].lower() == "x":
+                elif self.board[y][x].lower() == "2":
                     self.goal = (x, y)
 
         super(MazeSolver, self).__init__(initial_state=self.initial)
 
-    # Define the method that takes actions
-    # to arrive at the solution
     def actions(self, state):
         actions = []
         for action in COSTS.keys():
             newx, newy = self.result(state, action)
-            if self.board[newy][newx] != "#":
+            if self.board[newy][newx] != "*":
                 actions.append(action)
 
         return actions
 
-    # Update the state based on the action
     def result(self, state, action):
         x, y = state
 
@@ -47,15 +41,12 @@ class MazeSolver(SearchProblem):
 
         return new_state
 
-    # Check if we have reached the goal
     def is_goal(self, state):
         return state == self.goal
 
-    # Compute the cost of taking an action
     def cost(self, state, action, state2):
         return COSTS[action]
 
-    # Heuristic that we use to arrive at the solution
     def heuristic(self, state):
         x, y = state
         gx, gy = self.goal
@@ -64,59 +55,47 @@ class MazeSolver(SearchProblem):
 
 
 if __name__ == "__main__":
-    # Define the map
     MAP = """
-    ##############################
-    #         #              #   #
-    # ####    ########       #   #
-    #  o #    #              #   #
-    #    ###     #####  ######   #
-    #      #   ###   #           #
-    #      #     #   #  #  #   ###
-    #     #####    #    #  # x   #
-    #              #       #     #
-    ##############################
+    ******************************
+    *         *              *   *
+    * ****    ********       *   *
+    *  1 *    *              *   *
+    *    ***     *****  ******   *
+    *      *   ***   *           *
+    *      *     *   *  *  *   ***
+    *     *****    *    *  * 2   *
+    *              *       *     *
+    ******************************
     """
 
-    # Convert map to a list
     print(MAP)
     MAP = [list(x) for x in MAP.split("\n") if x]
 
-    # Define cost of moving around the map
     cost_regular = 1.0
     cost_diagonal = 1.7
 
-    # Create the cost dictionary
     COSTS = {
         "up": cost_regular,
         "down": cost_regular,
         "left": cost_regular,
         "right": cost_regular,
-        "up left": cost_diagonal,
-        "up right": cost_diagonal,
-        "down left": cost_diagonal,
-        "down right": cost_diagonal,
     }
 
-    # Create maze solver object
     problem = MazeSolver(MAP)
 
-    # Run the solver
     result = astar(problem, graph_search=True)
 
-    # Extract the path
     path = [x[1] for x in result.path()]
 
-    # Print the result
     print()
     for y in range(len(MAP)):
         for x in range(len(MAP[y])):
             if (x, y) == problem.initial:
-                print('o', end='')
+                print('1', end='')
             elif (x, y) == problem.goal:
-                print('x', end='')
+                print('2', end='')
             elif (x, y) in path:
-                print('·', end='')
+                print('0', end='')
             else:
                 print(MAP[y][x], end='')
 
